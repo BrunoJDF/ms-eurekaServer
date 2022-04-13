5869eb5582e89dcbab0036ef487114268de353d3
Bruno Diaz <brunjo12@hotmail.com>
2022-04-13 15:40:48 -0500

    creacion de servicio eureka discovery para registro de microservicios

---
+++ src/test/java/com/ms/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ms.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+
+
+}
